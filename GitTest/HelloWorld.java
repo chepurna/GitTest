1129627ee0ad4c7baee7a4833955803b2cd08605
Nav Che <nchepur28181@gmail.com>
2018-08-30 01:35:25 -0400

    removed a file

--- GitTest/HelloWorld.java
+++
@@ -1,8 +0,0 @@
-package org.learning;
-
-public class HelloWorld {
-public static void main(String[] args){
-	System.out.println("Hello World");
-	System.out.println("Pushed changes to Gh again");
-}
-}
